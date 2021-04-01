ebbc57163d81e9cd0a3f61f7db0281bfcb166d61
Svetlana <mokh-svetlana@yandex.ru>
2021-04-01 10:03:23 +0300

    finished burger menu

---
+++ app/js/js.js
@@ -0,0 +1,8 @@
+(function () {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.header__links');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger_active');
+        menu.classList.toggle('header__links_active');
+    });
+}());
